347a35d84c2be7b7d8df03b42d0c78398c774273
JadeChen <jie.chen1005@gmail.com>
2013-11-02 12:48:27 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -1,7 +1,7 @@
 (function ( $ ){
 
 	
-	EXAMPLE CONFIGURATION
+	//EXAMPLE CONFIGURATION
 
 		var defaultKey	= 'fje329iun52ngtuijo2f4jeun432A', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [61916,12425,94322], // Comma separated array of Xively Feed ID numbers
@@ -12,13 +12,14 @@
 		hideForm		= 0;
 	
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
+	/*var defaultKey		= '', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		*/
 
 // Function Declarations
 
