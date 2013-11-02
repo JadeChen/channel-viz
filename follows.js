cabbc3bcde9db43dc773346728085df17d6e0948
JadeChen <jie.chen1005@gmail.com>
2013-11-02 12:46:29 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -1,6 +1,6 @@
 (function ( $ ){
 
-	/*
+	
 	EXAMPLE CONFIGURATION
 
 		var defaultKey	= 'fje329iun52ngtuijo2f4jeun432A', // Unique master Xively API key to be used as a default
@@ -10,7 +10,7 @@
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0;
-	*/
+	
 
 	var defaultKey		= '', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
